e7e413b941e41b6a1d62e23e9655abc041bdd545
Siyoka Okaya <rajansahu.vedsar@gmail.com>
2025-02-03 11:17:05 +0530

    apis

--- src/site-components/Helper/Constant.jsx
+++ src/site-components/Helper/Constant.jsx
@@ -1,6 +1,5 @@
-export const MAIN_URL = "http://localhost:3000/";
-export const NODE_API_URL = "http://localhost:9000";
-// export const CKEDITOR_URL = "http://siyoka.com/ghanshyamurvashipgcollege/cp-panel/assets/ckeditor/ckeditor.js";
-export const CKEDITOR_URL = "https://ghanshyamurvashipgcollege.com/cp-panel/assets/ckeditor/ckeditor.js";
-export const PHP_API_URL = "http://localhost/nlu/backend/php-server/controller";
-export const FILE_API_URL = "http://localhost:9000/public/upload";
+export const MAIN_URL = "https://www.spaceshineone.co.in/";
+export const NODE_API_URL = "https://mymindgreatone.co.in";
+export const CKEDITOR_URL = "https://www.spaceshineone.co.in/ckeditor.js";
+export const PHP_API_URL = "https://www.spaceshineone.co.in/php-server/controller";
+export const FILE_API_URL = "https://www.spaceshineone.co.in/public/upload";

4200ea2edbf9f53283b16627f40ece03a2775b95
Siyoka Okaya <rajansahu.vedsar@gmail.com>
2025-02-04 10:40:57 +0530

    s

--- src/site-components/Helper/Constant.jsx
+++ src/site-components/Helper/Constant.jsx
@@ -10,9 +10,3 @@ export const FILE_API_URL = "https://www.spaceshineone.co.in/public/upload";
 // export const CKEDITOR_URL = "https://ghanshyamurvashipgcollege.com/cp-panel/assets/ckeditor/ckeditor.js";
 // export const PHP_API_URL = "http://localhost/nlu/backend/php-server/controller";
 // export const FILE_API_URL = "http://localhost:9000/public/upload";
-
-// export const MAIN_URL = "https://www.spaceshineone.co.in/";
-// export const NODE_API_URL = "http://localhost:2000";
-// export const CKEDITOR_URL = "https://www.spaceshineone.co.in/ckeditor/ckeditor.js";
-// export const PHP_API_URL = "https://www.spaceshineone.co.in/php-server/controller";
-// export const FILE_API_URL = "https://www.spaceshineone.co.in/public/upload";

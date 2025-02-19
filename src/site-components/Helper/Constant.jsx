7f8fa1eb1081c79d4b89012874d236c421c88b7a
Siyoka Okaya <rajansahu.vedsar@gmail.com>
2025-02-19 19:04:28 +0530

    s

--- src/site-components/Helper/Constant.jsx
+++ src/site-components/Helper/Constant.jsx
@@ -11,7 +11,6 @@ export const CKEDITOR_URL = "https://www.rpnlup.ac.in/ckeditor/ckeditor.js";
 export const PHP_API_URL = "https://www.rpnlup.ac.in/php-server/controller";
 export const FILE_API_URL = "https://www.rpnlup.ac.in/public/upload";
 
-
 // export const MAIN_URL = "http://localhost:3000/";
 // export const NODE_API_URL = "http://localhost:9000";
 // export const CKEDITOR_URL = "https://ghanshyamurvashipgcollege.com/cp-panel/assets/ckeditor/ckeditor.js";

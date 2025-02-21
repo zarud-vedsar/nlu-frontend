a0a29a8ada3f5a7806448d20099d7a083063b76c
Aman Yadav <amanyddv@gmail.com>
2025-02-21 13:45:56 +0530

    updateed book stock history

--- src/site-pages/admin/LibraryManagement/LibraryListing.jsx
+++ src/site-pages/admin/LibraryManagement/LibraryListing.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import Select from "react-select";
-import { Calendar } from "primereact/calendar";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import {
@@ -16,22 +15,12 @@ import {
   formatDate,
   goBack,
 } from "../../../site-components/Helper/HelperFunction";
-import { Link, useParams } from "react-router-dom";
 import { FormField } from "../../../site-components/admin/assets/FormField";
 
 function LibraryListing() {
-  const [bookOptions, setBookOptions] = useState([]);
-  const [userOptions, setUserOptions] = useState([]);
-  const [isbnOptions, setIsbnOptions] = useState([]);
-
-  const [selectedBook, setSelectedBook] = useState(null);
-  const [selectedUser, setSelectedUser] = useState(null);
-  const [selectedIsbn, setSelectedIsbn] = useState(null);
-  const [fromDate, setFromDate] = useState(null);
-  const [toDate, setToDate] = useState(null);
-
   const [tableData, setTableData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   const formatDateForMonth = (date) => {
     return new Intl.DateTimeFormat("en-CA", {
@@ -52,6 +41,7 @@ function LibraryListing() {
       new Date(now.getFullYear(), now.getMonth() + 1, 0)
     );
   };
+
   const filterData = {
     listing: "yes",
     book_id: "",
@@ -62,115 +52,44 @@ function LibraryListing() {
   };
   const [filters, setFilters] = useState(filterData);
   useEffect(() => {
-    fetchTableData();
+    fetchTableData(filterData);
   }, []);
-  const [isFetching, setIsFetching] = useState(false);
-  const fetchTableData = async () => {
+
+  const fetchTableData = async (filter) => {
     setIsFetching(true);
-    
 
     try {
       const response = await axios.post(
         `${NODE_API_URL}/api/library-management/fetch-book-stock-history`,
-        {...filters}
+        { ...filter }
       );
       if (response.data?.statusCode === 200) {
         const data = response.data.data;
 
-        setTableData(data);
         setFilteredData(data);
-        processDropdownOptions(data);
+        console.log(tableData.length);
+        if (tableData && tableData.length === 0) {
+          setTableData(data);
+        }
       } else {
         toast.error("No data found.");
       }
     } catch (error) {
+      setFilteredData([]);
       toast.error("Error fetching table data.");
     } finally {
       setIsFetching(false);
     }
   };
 
-  const processDropdownOptions = (data) => {
-    const uniqueBooks = [
-      ...new Map(data.map((item) => [item.book_id, item])).values(),
-    ];
-    const uniqueUsers = [
-      ...new Map(data.map((item) => [item.user_id, item])).values(),
-    ];
-    const uniqueIsbns = [
-      ...new Map(data.map((item) => [item.isbn_no, item])).values(),
-    ];
-
-    setBookOptions(
-      uniqueBooks.map((book) => ({
-        label: book.book_name,
-        value: book.book_id,
-      }))
-    );
-    setUserOptions(
-      uniqueUsers.map((user) => ({
-        label: `${user.first_name} ${user.last_name}`,
-        value: user.user_id,
-      }))
-    );
-    setIsbnOptions(
-      uniqueIsbns.map((isbn) => ({ label: isbn.isbn_no, value: isbn.isbn_no }))
-    );
-  };
-
-  const handleSearch = () => {
-    let filtered = tableData;
-
-    if (selectedBook) {
-      filtered = filtered.filter((item) => item.book_id === selectedBook.value);
-    }
-    if (selectedUser) {
-      filtered = filtered.filter((item) => item.user_id === selectedUser.value);
-    }
-    if (selectedIsbn) {
-      filtered = filtered.filter((item) => item.isbn_no === selectedIsbn.value);
-    }
-    if (fromDate) {
-      filtered = filtered.filter(
-        (item) => new Date(item.issue_date) >= fromDate
-      );
-    }
-    if (toDate) {
-      filtered = filtered.filter((item) => new Date(item.issue_date) <= toDate);
-    }
-
-    setFilteredData(filtered);
-  };
-
   const handleReset = () => {
-    setSelectedBook(null);
-    setSelectedUser(null);
-    setSelectedIsbn(null);
-    setFromDate(null);
-    setToDate(null);
-    setFilteredData(tableData);
+    setFilters(filterData);
+    fetchTableData(filterData);
   };
-  const facultyTemplate = (rowData) => {
-    return (
-      <>
-        <div className="row">
-          <div className=" col-md-2">
-            <img
-              src={`${FILE_API_URL}/books/${rowData.userImage}`}
-              alt=""
-              className=""
-            />
-          </div>{" "}
-          <div className="col-md-8">
-            <div>
-              {rowData.first_name} {rowData.last_name}
-            </div>
-            <div className="text-sm text-gray-500">UID: {rowData.uid}</div>
-          </div>
-        </div>
-      </>
-    );
+  const handleSearch = () => {
+    fetchTableData(filters);
   };
+
   return (
     <div className="page-container">
       <div className="main-content">
@@ -203,32 +122,76 @@ function LibraryListing() {
         </div>
         <div className="card p-4">
           <div className="row">
-            <div className="col-md-4">
+            <div className="col-md-3">
               <label>Select Book</label>
               <Select
-                options={bookOptions}
-                value={selectedBook}
-                onChange={setSelectedBook}
+                options={Array.from(
+                  new Map(
+                    tableData.map((data) => [data.book_id, data])
+                  ).values()
+                ).map((data) => ({
+                  value: data.book_id,
+                  label: `${data.book_name} - ${data.isbn_no}`,
+                }))}
+                value={
+                  tableData.find((data) => data.book_id === filters?.book_id)
+                    ? {
+                        value: filters?.book_id,
+                        label: `${
+                          tableData.find(
+                            (data) => data.book_id === filters?.book_id
+                          ).book_name
+                        } - ${
+                          tableData.find(
+                            (data) => data.book_id === filters?.book_id
+                          ).isbn_no
+                        }`,
+                      }
+                    : { value: null, label: "Select" }
+                }
+                onChange={(selectedOption) => {
+                  setFilters((prev) => ({
+                    ...prev,
+                    book_id: selectedOption.value,
+                  }));
+                }}
               />
             </div>
-            <div className="col-md-4">
-              <label>Select User</label>
+            <div className="col-md-3">
+              <label>Select Employe</label>
               <Select
-                options={userOptions}
-                value={selectedUser}
-                onChange={setSelectedUser}
+                options={Array.from(
+                  new Map(
+                    tableData.map((data) => [data.user_id, data])
+                  ).values()
+                ).map((data) => ({
+                  value: data.user_id,
+                  label: `${data.first_name}  ${data.last_name}`,
+                }))}
+                value={
+                  tableData.find((data) => data.user_id === filters?.user_id)
+                    ? {
+                        value: filters?.user_id,
+                        label: `${
+                          tableData.find(
+                            (data) => data.user_id === filters?.user_id
+                          ).first_name
+                        } ${
+                          tableData.find(
+                            (data) => data.user_id === filters?.user_id
+                          ).last_name
+                        }`,
+                      }
+                    : { value: null, label: "Select" }
+                }
+                onChange={(selectedOption) => {
+                  setFilters((prev) => ({
+                    ...prev,
+                    user_id: selectedOption.value,
+                  }));
+                }}
               />
             </div>
-            <div className="col-md-4">
-              <label>Select ISBN</label>
-              <Select
-                options={isbnOptions}
-                value={selectedIsbn}
-                onChange={setSelectedIsbn}
-              />
-            </div>
-          </div>
-          <div className="row mt-3">
             <FormField
               label="From Date"
               name="fromDate"
@@ -237,7 +200,7 @@ function LibraryListing() {
               value={filters.fromDate}
               column="col-md-3 col-lg-3"
               onChange={(e) =>
-                setFormData((prev) => ({
+                setFilters((prev) => ({
                   ...prev,
                   fromDate: e.target.value,
                 }))
@@ -251,13 +214,15 @@ function LibraryListing() {
               value={filters.toDate}
               column="col-md-3 col-lg-3"
               onChange={(e) =>
-                setFormData((prev) => ({
+                setFilters((prev) => ({
                   ...prev,
                   toDate: e.target.value,
                 }))
               }
             />
-            <div className="col-md-4 mt-4 pt-1 ">
+          </div>
+          <div className="row ">
+            <div className="col-md-4 pt-1 ">
               <button
                 onClick={handleSearch}
                 className="btn btn-dark mr-2"
@@ -295,25 +260,118 @@ function LibraryListing() {
                     sortable
                   />
 
-                  <Column field="isbn_no" header="ISBN Number" sortable />
+                  
                   <Column
-                    header="Faculty Name"
-                    body={facultyTemplate}
+                    header="BooK"
+                    body={(rowData) => (
+                      <div
+                        className="info-column d-flex align-items-center
+                  "
+                      >
+                        <div className="info-image mr-4">
+                          {rowData.bookImage ? (
+                            <img
+                              src={`${FILE_API_URL}/books/${rowData.bookImage}`}
+                              alt=""
+                              style={{
+                                width: "40px",
+                                height: "40px",
+                                backgroundColor: "#e6fff3",
+                                fontSize: "20px",
+                                color: "#00a158",
+                              }}
+                              className="rounded-circle d-flex justify-content-center align-items-center"
+                            />
+                          ) : (
+                            <div
+                              style={{
+                                width: "40px",
+                                height: "40px",
+                                backgroundColor: "#e6fff3",
+                                fontSize: "20px",
+                                color: "#00a158",
+                              }}
+                              className="rounded-circle d-flex justify-content-center align-items-center"
+                            >
+                              {rowData?.book_name[0]}
+                            </div>
+                          )}
+                        </div>
+                        <div>
+                          <div className="info-name">
+                            <span>{`${rowData.book_name} `}</span>
+                          </div>
+                        </div>
+                      </div>
+                    )}
+                    field="first_name"
+                    sortable
                     style={{ minWidth: "17rem" }}
                   />
                   <Column
-                    field="book_name"
-                    header="Book Name"
+                    header="Employe"
+                    body={(rowData) => (
+                      <div
+                        className="info-column d-flex align-items-center
+                  "
+                      >
+                        <div className="info-image mr-4">
+                          {rowData.userImage ? (
+                            <img
+                              src={`${FILE_API_URL}/user/${rowData.uid}/${rowData.userImage}`}
+                              alt=""
+                              style={{
+                                width: "40px",
+                                height: "40px",
+                                backgroundColor: "#e6fff3",
+                                fontSize: "20px",
+                                color: "#00a158",
+                              }}
+                              className="rounded-circle d-flex justify-content-center align-items-center"
+                            />
+                          ) : (
+                            <div
+                              style={{
+                                width: "40px",
+                                height: "40px",
+                                backgroundColor: "#e6fff3",
+                                fontSize: "20px",
+                                color: "#00a158",
+                              }}
+                              className="rounded-circle d-flex justify-content-center align-items-center"
+                            >
+                              {rowData?.first_name[0]}
+                            </div>
+                          )}
+                        </div>
+                        <div>
+                          <div className="info-name">
+                            <span>{`${rowData.first_name} ${rowData.last_name}`}</span>
+                          </div>
+
+                          <div className="info-email">
+                            <span>{rowData.uid}</span>
+                          </div>
+                        </div>
+                      </div>
+                    )}
+                    field="first_name"
                     sortable
-                    style={{ minWidth: "15rem" }}
+                    style={{ minWidth: "17rem" }}
                   />
+
+<Column field="isbn_no" header="ISBN Number" sortable />
+                  
+
+                  
+                  <Column field="qty" header="Quantity" sortable />
+                  <Column field="vendor" header="Vendor" sortable />
                   <Column
                     field="description"
-                    header="Description"
+                    header="Remark"
                     sortable
                     style={{ minWidth: "15rem" }}
                   />
-                  <Column field="qty" header="Quantity" sortable />
                   <Column
                     field="created_at"
                     header="Created At"

3dc0c050322f4b66ea3993a55a318fe148c9c442
Dilshadkhan08 <mddilshad3997@gmail.com>
2025-02-21 17:47:27 +0530

    update

--- src/site-pages/website/AllNotice.jsx
+++ src/site-pages/website/AllNotice.jsx
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { NODE_API_URL } from '../../site-components/Helper/Constant';
 import { useParams } from 'react-router-dom';
+import validator from "validator";
+import { FaArrowRightLong, FaAnglesDown } from "react-icons/fa6";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 const AllNotice = () => {
   const [AllNotice, setAllNotice] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -58,7 +61,7 @@ const AllNotice = () => {
       </div>
 
 
-      <div className="about-page-area section-padding">
+      <div className="about-page-area section-padding ">
         <div className="container">
           <div className="row">
             <div className="col-md-12">
@@ -74,18 +77,22 @@ const AllNotice = () => {
           </div>
 
           {/* Display loading indicator while data is being fetched */}
-          {loading ? (
+          {/* {loading ? (
             <div>Loading...</div>
-          ) : (
+          ) : ( */}
             <div className="row">
-              <div className="col-md-12">
+              {/* <div className="col-md-12">
                 <div className="notic-container">
                   <div id="news-head">
                     Total Notices: <span className="nott">{AllNotice.length}</span>
                   </div>
+                  </div>
+                  </div> */}
+
+
 
                   {/* Dynamically map over AllNotice array */}
-                  {AllNotice.map((notice, index) => (
+                  {/* {AllNotice.map((notice, index) => (
                     <div key={index} className="noricerow">
 
                       <div className='noticecontent'>
@@ -106,11 +113,67 @@ const AllNotice = () => {
                       </div>
 
                     </div>
-                  ))}
+                  ))} */}
+
+                   {AllNotice.map((notice, index) => (
+                                        <div key={index} className="col-lg-3">
+                                          <div className="card border-0 soft-shadow  ">
+                                            <div className="new-img-container">
+                                              <Link to={`/notice-details/${notice.id}`}>
+                                                <img
+                                                  src={notice.image || placeholder}
+                                                  className="news-image"
+                                                  alt="News Image"
+                                                />
+                                              </Link>
+                                            </div>
+                                            <div className="card-body">
+                                              <div className="card-text gorditas-regular text-center id-event-date">
+                                                <p className="date-month mb-0">
+                                                  {new Date(
+                                                    notice.notice_date
+                                                  ).toLocaleDateString("en-US", {
+                                                    month: "short",
+                                                  })}{" "}
+                                                  {/* Month */}
+                                                </p>
+                                                <h5 className="date-day">
+                                                  {new Date(
+                                                    notice.notice_date
+                                                  ).toLocaleDateString("en-US", {
+                                                    day: "2-digit",
+                                                  })}{" "}
+                                                  {/* Day */}
+                                                </h5>
+                                                <p className="date-year mb-0">
+                                                  {new Date(
+                                                    notice.notice_date
+                                                  ).toLocaleDateString("en-US", {
+                                                    year: "numeric",
+                                                  })}{" "}
+                                                  {/* Year */}
+                                                </p>
+                                              </div>
+                                              <div className="newsttl">
+                                                {validator.unescape(notice.title)}
+                                              </div>
+                                            </div>
+                                            <div className="cardftr">
+                                              <Link
+                                                className="btn btn-primary border border-primary d-flex justify-content-center align-items-center w-fit about-read-more"
+                                                to={`/notice-details/${notice.id}`}
+                                              >
+                                                Read More &nbsp; <FaArrowRightLong />
+                                              </Link>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      ))}
+
                 </div>
-              </div>
-            </div>
-          )}
+             
+            
+          {/* )} */}
         </div>
       </div>
     </>

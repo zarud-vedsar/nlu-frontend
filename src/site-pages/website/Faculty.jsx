66d4abfe0300ff0d399cc2a7b80bb1eea5902824
Siyoka Okaya <rajansahu.vedsar@gmail.com>
2025-02-25 15:34:00 +0530

    s

--- src/site-pages/website/Faculty.jsx
+++ src/site-pages/website/Faculty.jsx
@@ -10,62 +10,77 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { BiEnvelope, BiPhone } from "react-icons/bi";
 import validator from 'validator';
-import f1 from './fimages/f1.jpg';
-import f2 from './fimages/f2.jpg';
-import f3 from './fimages/f3.jpg';
-import f4 from './fimages/f4.jpg';
-import f5 from './fimages/f5.jpg';
-import f6 from './fimages/f6.jpg';
-import f7 from './fimages/f7.jpg';
+import f1 from './fimage/f1.jpg';
+import f2 from './fimage/f2.jpg';
+import f3 from './fimage/f3.jpg';
+import f4 from './fimage/f4.jpg';
+import f5 from './fimage/f5.jpg';
+import f6 from './fimage/f6.jpg';
+import f7 from './fimage/f7.jpg';
 const Faculty = () => {
   const [facultyList, setFacultyList] = useState([
     {
       avtar: f1,
-      uid: "RPNLUP",
       first_name: "Sr. Prof. Dr. Usha ",
       last_name: "Tandon",
-      designation: "Vice Chancellor",
+      designation: "Vice-Chancellor",
+      email1: 'usha@rpnlup.ac.in',
+      email2: 'vc@rpnlup.ac.in',
       id: 1
     },
     {
-      avtar: f1,
-      uid: "RPNLUP",
-      first_name: "Sr. Prof. Dr. Usha ",
-      last_name: "Tandon",
-      designation: "Vice Chancellor",
-      id: 1
+      avtar: f2,
+      first_name: "Dr. Deepak ",
+      last_name: "Sharma",
+      designation: "Assistant Professor of Law",
+      email1: 'deepak@rpnlup.ac.in',
+      email2: '',
+      id: 2
     },
     {
-      avtar: f1,
-      uid: "RPNLUP",
-      first_name: "Sr. Prof. Dr. Usha ",
-      last_name: "Tandon",
-      designation: "Vice Chancellor",
-      id: 1
+      avtar: f3,
+      first_name: "Dr. Sonika",
+      last_name: "",
+      designation: "Assistant Professor of Law",
+      email1: 'sonika@rpnlup.ac.in',
+      email2: '',
+      id: 3
     },
     {
-      avtar: f1,
-      uid: "RPNLUP",
-      first_name: "Sr. Prof. Dr. Usha ",
-      last_name: "Tandon",
-      designation: "Vice Chancellor",
-      id: 1
+      avtar: f4,
+      first_name: "Dr. Prakash ",
+      last_name: "Tripathi",
+      designation: "Assistant Professor of Sociology",
+      email1: 'prakash@rpnulp.ac.in',
+      email2: '',
+      id: 4
     },
     {
-      avtar: f1,
-      uid: "RPNLUP",
-      first_name: "Sr. Prof. Dr. Usha ",
-      last_name: "Tandon",
-      designation: "Vice Chancellor",
-      id: 1
+      avtar: f5,
+      first_name: "Dr. Neha ",
+      last_name: "Dubey",
+      designation: "Assistant Professor of English",
+      email1: 'neha@rpnlup.ac.in',
+      email2: '',
+      id: 5
     },
     {
-      avtar: f1,
-      uid: "RPNLUP",
-      first_name: "Sr. Prof. Dr. Usha ",
-      last_name: "Tandon",
-      designation: "Vice Chancellor",
-      id: 1
+      avtar: f6,
+      first_name: "Dr. Suchit ",
+      last_name: "Kumar Yadav",
+      designation: "Assistant Professor of Political Science",
+      email1: 'suchit@rpnlup.ac.in',
+      email2: '',
+      id: 6
+    },
+    {
+      avtar: f6,
+      first_name: "Dr. Akanshi ",
+      last_name: "Vidyarthi",
+      designation: "Assistant Professor of History",
+      email1: 'akanshi@rpnlup.ac.in',
+      email2: '',
+      id: 6
     },
   ]);
 
@@ -85,14 +100,14 @@ const Faculty = () => {
           },
         }
       );
-      setFacultyList((prev) => [...prev, ...response.data.data]);
+      // setFacultyList((prev) => [...prev, ...response.data.data]);
     } catch (error) {
       // Handle errors (empty for now)
     }
   };
 
   useEffect(() => {
-    loadFacultyData();
+    // loadFacultyData();
   }, []);
 
   const moreDetail = (id) => {
@@ -140,13 +155,9 @@ const Faculty = () => {
                         style={{ height: "300px" }}
                       >
                         <img
-                          style={{ width: "300px", height: "300px" }}
+                          style={{ width: "300px", height: "260px" }}
                           className="mx-auto"
-                          src={
-                            faculty.avtar
-                              ? `${FILE_API_URL}/user/${faculty.uid}/${faculty.avtar}`
-                              : `${FILE_API_URL}/user/dummy.webp`
-                          }
+                          src={faculty.avtar}
                           alt=""
                         />
                       </div>

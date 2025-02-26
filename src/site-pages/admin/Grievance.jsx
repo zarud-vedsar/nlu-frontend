4076ad801e50b03e3ef2f0ba5c23c22780dd9a37
Aman Yadav <amanyddv@gmail.com>
2025-02-26 11:59:41 +0530

    updated

--- src/site-pages/admin/Grievance.jsx
+++ src/site-pages/admin/Grievance.jsx
@@ -3,12 +3,7 @@ import axios, { all } from "axios";
 import {
   Modal,
   Button,
-  Form,
-  Table,
   Spinner,
-  Col,
-  Row,
-  InputGroup,
 } from "react-bootstrap";
 
 import { FaArrowLeft } from "react-icons/fa6";
@@ -16,7 +11,6 @@ import { FaArrowLeft } from "react-icons/fa6";
 import "../../site-components/admin/assets/css/FacultyList.css";
 import {
   NODE_API_URL,
-  PHP_API_URL,
 } from "../../site-components/Helper/Constant";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/Column";
@@ -24,11 +18,9 @@ import { InputText } from "primereact/inputtext"; // Import InputText for the se
 import "../../../node_modules/primeicons/primeicons.css";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import Tooltip from "react-bootstrap/Tooltip";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import secureLocalStorage from "react-secure-storage";
-import { toast } from "react-toastify";
 import { formatDate } from "../../site-components/Helper/HelperFunction";
-import { end } from "@popperjs/core";
 
 function MyVerticallyCenteredModal(props) {
   return (
@@ -62,48 +54,12 @@ const Grievance = () => {
   const [allGrievanceList, setAllGrievanceList] = useState([]);
   const [globalFilter, setGlobalFilter] = useState("");
   const [modalShow, setModalShow] = useState(false);
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
+
   const [selectedGrievance, setSelectedGrievance] = useState(null);
   const [loading, setLoading] = useState(false);
-
-  const filterByDateRange = (startDate, endDate) => {
-    const start = startDate ? new Date(startDate) : null;
-    const end = endDate ? new Date(endDate) : null;
-
-    // Stripping time part from the start and end date (set hours to 00:00:00)
-    if (start) {
-      start.setHours(0, 0, 0, 0);
-    }
-    if (end) {
-      end.setHours(23, 59, 59, 999);
-    }
-
-
-    let data = allGrievanceList.filter((item) => {
-      const createdAtDate = new Date(item.created_at);
-      createdAtDate.setHours(0, 0, 0, 0);
-      if (start && end) {
-        return createdAtDate >= start && createdAtDate <= end;
-      }
-      if (start) {
-        return createdAtDate >= start;
-      }
-      if (end) {
-        return createdAtDate <= end;
-      }
-      return true;
-    });
-
-
-    setGrievanceList(data);
-  };
+  const [formData, setFormData] = useState({ startDate: "", endDate: "" });
 
   useEffect(() => {
-    filterByDateRange(startDate, endDate);
-  }, [startDate, endDate]);
-
-  const initializeDate = () => {
     const today = new Date();
     const start = new Date(today);
     start.setDate(today.getDate() - 7);
@@ -111,14 +67,11 @@ const Grievance = () => {
     const formattedStartDate = start.toISOString().split("T")[0];
     const formattedEndDate = today.toISOString().split("T")[0];
 
-    setStartDate(formattedStartDate);
-    setEndDate(formattedEndDate);
-  };
+    setFormData({ startDate: formattedStartDate, endDate: formattedEndDate });
+  }, []);
 
   useEffect(() => {
     loadGrievanceData();
-
-    initializeDate();
   }, []);
 
   useEffect(() => {
@@ -138,7 +91,8 @@ const Grievance = () => {
       const bformData = new FormData();
       bformData.append("loguserid", secureLocalStorage.getItem("login_id"));
       bformData.append("login_type", secureLocalStorage.getItem("loginType"));
-
+      bformData.append("startDate",formData?.startDate);
+      bformData.append("endDate",formData?.endDate);
       const response = await axios.post(
         `${NODE_API_URL}/api/grievance/list`,
         bformData,
@@ -149,8 +103,8 @@ const Grievance = () => {
         }
       );
       setGrievanceList(response.data.data);
-      setAllGrievanceList(response.data.data);
     } catch (error) {
+      setGrievanceList([])
     } finally {
       setLoading(false);
     }
@@ -160,143 +114,172 @@ const Grievance = () => {
     <>
       <div className="page-container">
         <div className="main-content">
-        <div className="container-fluid">
-          <div className="">
-            <nav className="breadcrumb">
-            <a href="/admin/home" className="breadcrumb-item">
+          <div className="container-fluid">
+            <div className="">
+              <nav className="breadcrumb">
+                <a href="/admin/home" className="breadcrumb-item">
                   <i className="fas fa-home m-r-5" /> Dashboard
                 </a>
 
-              <span className="breadcrumb-item active">Inquiry</span>
+                <span className="breadcrumb-item active">Inquiry</span>
 
-              <span className="breadcrumb-item active">Grievance</span>
-            </nav>
-          </div>
+                <span className="breadcrumb-item active">Grievance</span>
+              </nav>
+            </div>
 
-          <div className="card bg-transparent mb-2">
+            <div className="card bg-transparent mb-2">
               <div className="card-header d-flex justify-content-between align-items-center px-0">
                 <h5 className="card-title h6_new">Grievance</h5>
                 <div className="ml-auto">
-                <Button
-                variant="light"
-                onClick={() => window.history.back()}
-                className=" mb-md-0"
-              >
-                <i className="fas">
-                  <FaArrowLeft />
-                </i>{" "}
-                Go Back
-              </Button>
+                  <Button
+                    variant="light"
+                    onClick={() => window.history.back()}
+                    className=" mb-md-0"
+                  >
+                    <i className="fas">
+                      <FaArrowLeft />
+                    </i>{" "}
+                    Go Back
+                  </Button>
                 </div>
               </div>
             </div>
-          
-          <div className="card">
-            <div className="card-body">
-              <div className="row">
-                <div className="col-md-7 col-lg-7 col-7 col-sm-7 p-input-icon-left mb-3 d-flex justify-content-start align-items-center">
-                  <div className="search-icon">
-                    <i className="pi pi-search" />
+
+            <div className="card">
+              <div className="card-body">
+                <div className="row d-flex align-items-end">
+                  <div className="col-md-5 col-lg-5 col-12 col-sm-12 p-input-icon-left  d-flex justify-content-start align-items-center">
+                    <div className="search-icon">
+                      <i className="pi pi-search" />
+                    </div>
+                    <InputText
+                      type="search"
+                      value={globalFilter}
+                      onChange={(e) => setGlobalFilter(e.target.value)}
+                      placeholder="Search"
+                      className="form-control dtsearch-input"
+                    />
                   </div>
-                  <InputText
-                    type="search"
-                    value={globalFilter}
-                    onChange={(e) => setGlobalFilter(e.target.value)}
-                    placeholder="Search"
-                    className="form-control dtsearch-input"
-                  />
-                </div>
-                <div className="col-md-2 col-lg-2 col-10 col-sm-4 mr-3  d-flex align-items-center">
-                  <label htmlFor="start-date" className="mr-2">
-                    Start Date
-                  </label>
-                  <input
-                    type="date"
-                    id="start-date"
-                    value={startDate}
-                    className="form-control"
-                    onChange={(e) => setStartDate(e.target.value)}
-                  />
+                  <div className="col mr-3 ">
+                    <label htmlFor="start-date" className="mr-2">
+                      Start Date
+                    </label>
+                    <input
+                      type="date"
+                      id="start-date"
+                      value={formData?.startDate}
+                      className="form-control"
+                      onChange={(e) => setFormData((prev)=>({...prev,startDate:e.target.value}))}
+                    />
+                  </div>
+
+                  <div className="col ">
+                    <label htmlFor="end-date" className="mr-2">
+                      End Date
+                    </label>
+                    <input
+                      type="date"
+                      id="end-date"
+                      value={formData?.endDate}
+                      className="form-control"
+                      onChange={(e) => setFormData((prev)=>({...prev,endDate:e.target.value}))}
+                    />
+                  </div>
+                  <div className="">
+                    <button className="btn btn-secondary" onClick={loadGrievanceData}>
+                      Fetch
+                    </button>
+                    </div>
+
                 </div>
 
-                <div className="col-md-2 col-lg-2 col-10 col-sm-4  d-flex align-items-center">
-                  <label htmlFor="end-date" className="mr-2">
-                    End Date
-                  </label>
-                  <input
-                    type="date"
-                    id="end-date"
-                    value={endDate}
-                    className="form-control"
-                    onChange={(e) => setEndDate(e.target.value)}
-                  />
-                </div>
+                {loading ? (
+                  <div className="text-center">
+                    <Spinner animation="border" />
+                  </div>
+                ) : (
+                  <div className="table-responsive mt-3">
+                    <DataTable
+                      value={grievanceList}
+                      paginator
+                      rows={10}
+                      rowsPerPageOptions={[10, 25, 50]}
+                      globalFilter={globalFilter}
+                      emptyMessage="No records found"
+                      className="p-datatable-custom"
+                      tableStyle={{ minWidth: "50rem" }}
+                      sortMode="multiple"
+                    >
+                      <Column
+                        body={(rowData, { rowIndex }) => rowIndex + 1}
+                        header="#"
+                        sortable
+                      />
+
+                      <Column field="name" header="Name" sortable />
+
+                      <Column field="email" header="Email" sortable />
+                      <Column
+                        field="year_semester"
+                        header="Semester"
+                        sortable
+                      />
+                      <Column field="phone" header="Contact" sortable />
+                      <Column
+                        field="created_at"
+                        header="Date"
+                        sortable
+                        body={(rowData) => `${formatDate(rowData.created_at)}`}
+                      />
+
+                      <Column
+                        header="Action"
+                        body={(rowData, { rowIndex }) => (
+                          <div className="d-flex ">
+                            <OverlayTrigger
+                              placement="bottom"
+                              overlay={
+                                <Tooltip id="button-tooltip-2">
+                                  View Grievance
+                                </Tooltip>
+                              }
+                            >
+                              <div className="avatar avatar-icon avatar-md avatar-orange mr-2">
+                                <i
+                                  className="fa-solid fa-eye"
+                                  onClick={() => viewAllImages(rowIndex)}
+                                ></i>
+                              </div>
+                            </OverlayTrigger>
+
+                            {rowData?.pdf_file && (
+                              <OverlayTrigger
+                                placement="bottom"
+                                overlay={
+                                  <Tooltip id="button-tooltip-2">
+                                    View File
+                                  </Tooltip>
+                                }
+                              >
+                                <Link
+                                  className="avatar avatar-icon avatar-md avatar-orange"
+                                  to={`${NODE_API_URL}/public/upload/${rowData?.pdf_file}`}
+                                  target="_blank"
+                                >
+                                  <i className="fa-solid fa-file"></i>
+                                </Link>
+                              </OverlayTrigger>
+                            )}
+                          </div>
+                        )}
+                        sortable
+                      />
+                    </DataTable>
+                  </div>
+                )}
               </div>
-
-              {loading ? (
-                <div className="text-center">
-                  <Spinner animation="border" />
-                </div>
-              ) : (
-                <div className="table-responsive mt-3">
-                  <DataTable
-                    value={grievanceList}
-                    paginator
-                    rows={10}
-                    rowsPerPageOptions={[10, 25, 50]}
-                    globalFilter={globalFilter}
-                    emptyMessage="No records found"
-                    className="p-datatable-custom"
-                    tableStyle={{ minWidth: "50rem" }}
-                    sortMode="multiple"
-                  >
-                    <Column
-                      body={(rowData, { rowIndex }) => rowIndex + 1}
-                      header="#"
-                      sortable
-                    />
-
-                    <Column field="name" header="Name" sortable />
-
-                    <Column field="email" header="Email" sortable />
-                    <Column field="year_semester" header="Semester" sortable />
-                    <Column field="phone" header="Contact" sortable />
-                    <Column
-                      field="created_at"
-                      header="Date"
-                      sortable
-                      body={(rowData) => `${formatDate(rowData.created_at)}`}
-                    />
-
-                    <Column
-                      header="Action"
-                      body={(rowData, { rowIndex }) => (
-                        <div className="d-flex justify-content-around">
-                          <OverlayTrigger
-                            placement="bottom"
-                            overlay={
-                              <Tooltip id="button-tooltip-2">
-                                View Grievance
-                              </Tooltip>
-                            }
-                          >
-                            <div className="avatar avatar-icon avatar-md avatar-orange">
-                              <i
-                                className="fa-solid fa-eye"
-                                onClick={() => viewAllImages(rowIndex)}
-                              ></i>
-                            </div>
-                          </OverlayTrigger>
-                        </div>
-                      )}
-                      sortable
-                    />
-                  </DataTable>
-                </div>
-              )}
             </div>
           </div>
-        </div>
         </div>
       </div>
       <MyVerticallyCenteredModal

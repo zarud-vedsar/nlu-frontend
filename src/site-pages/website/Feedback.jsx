6c01933c7fe3d6d088b398330439ec8bca6b5616
Dilshadkhan08 <mddilshad3997@gmail.com>
2025-02-26 18:33:06 +0530

    u

--- src/site-pages/website/Feedback.jsx
+++ src/site-pages/website/Feedback.jsx
@@ -105,13 +105,15 @@ const Feedback = () => {
       <section className="section bg-f5">
         <div className="container">
           <div className="row">
-            <div className="col-lg-7 col-md-7 col-sm-12 col-xs-12">
+            <div className="col-lg-7 col-md-7 col-sm-12 col-xs-12" style={{display:"flex", justifyContent:"center"}}>
               <img
                 className="img-fluid rounded-3 mt-5"
                 src={feedbackPng}
                 alt="Institution"
                 style={{
                   display: "block",
+                  height:"650px",
+                  // width:"100%",
                 }}
               />
             </div>
